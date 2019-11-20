bf3c0f9fa472434e67abb8aa8aeacb7b065d6c19
shogo-naga8 <x08smjkcrXxx@gmail.com>
2019-11-20 12:59:54 +0900

    イベント時に非同期通信できるようにする

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,25 +1,54 @@
-// $(function(){
-//   $("#user-search-field").on("keyup", function() {
-//     var input = $("#user-search-field").val();
-
-//     $.ajax({
-//       type: 'GET',
-//       url: '/users',
-//       data: { keyword: input },
-//       dataType: 'json'
-//     })
-//     .done(function(users) {
-//       console.log("成功です");
-//     })
-//     .fail(function() {
-//       console.log("失敗です");
-//     });
-//   });
-// });
-
 $(function() {
-  $("#group_name").on("keyup", function() {
-    let input = $("#group_name").val();
-    console.log(input);
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".ChatMembers").append(html);
+  }
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    console.log("発火");
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
+      });
   });
 });
